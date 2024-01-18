217b831b3ea1507d057f77734b8a9c78a2aa016e
Philipp Friedrich <wk279@uni-heidelberg.de>
2024-01-18 17:02:53 +0100

    Update models

--- src/main.py
+++ src/main.py
@@ -3,16 +3,16 @@ from PIL import Image
 import numpy as np
 
 # Load the ONNX model
-model_path = 'src/Standard_est_ckpt.onnx'
+model_path = 'src/course_best_ckpt.onnx'
 session = onnxruntime.InferenceSession(model_path)
 
 # Load and preprocess the image
-image_path = '../data/20230514/field_1_allPictures_(1500x2000)_cut_to_512p/IMG_20230514_155216_10.jpg'
+image_path = '../data/custom_dataset/images/train/17.67821.44655.png'
 image = Image.open(image_path)
 
 # Preprocess the image (replace with your own preprocessing logic)
 # For example, resizing the image to match the expected input size
-input_size = (512, 512)
+input_size = (256, 256)
 image = image.resize(input_size)
 input_data = np.array(image, dtype=np.float32)
 input_data = np.transpose(input_data, (2, 0, 1))  # Channels-first format if needed
