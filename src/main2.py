ddca68a1142a85f2d8f5b452d3754dbb8a929f29
Philipp Friedrich <wk279@uni-heidelberg.de>
2024-02-15 12:11:15 +0100

    Initial update main2.py

---
+++ src/main2.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8n.pt')  # pretrained YOLOv8n model
+
+# Run batched inference on a list of images
+results = model(['im1.jpg', 'im2.jpg'])  # return a list of Results objects
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    result.show()  # display to screen
+    result.save(filename='result.jpg')  # save to disk
