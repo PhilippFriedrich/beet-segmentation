a3a1f05a1ff88b6c44f4ad5d646ffd50544bed42
gustav <gustav.schimmer01@stud.uni-heidelberg.de>
2024-02-15 16:36:48 +0100

    changed files

--- src/main2.py
+++ src/main2.py
@@ -7,7 +7,4 @@ model = YOLO('yolov6n.yaml')
 model.info()
 
 # Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data='YOLOv6/data/dataset.yaml', epochs=10, imgsz=256)
-
-# Run inference with the YOLOv6n model on the 'bus.jpg' image
-results = model('data/custom_dataset/images/val/IMG_20230514_155238_26.jpg')
+results = model.train(data='../YOLOv6/data/dataset.yaml', epochs=10, imgsz=512)
--- src/main3.py
+++ src/main3.py
@@ -2,13 +2,15 @@ from ultralytics import YOLO
 import os
 
 # Load a model
-model = YOLO('runs/detect/train6/weights/best.pt')  # pretrained YOLOv8n model
+model = YOLO('runs/detect/train2/weights/best.pt')  # pretrained YOLOv8n model
 
 # Define the input image folder
-input_folder = 'data/custom_dataset/images/train/'
+input_folder = r"C:\Users\Gustav Schimmer\Desktop\Studium\Semester_3\deepLearnung\BeetSegmentation\beet-segmentation\data\zugeschnitteneUndGeoreferenzierteBilder"
 
 # Get a list of image files in the input folder
-image_files = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.endswith(('.jpg', '.jpeg', '.png'))]
+image_files = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.endswith(('.jpg', '.png', '.tiff', '.tif'))]
+
+i = 0
 
 # Process each image in the input folder
 for image_file in image_files:
@@ -16,12 +18,14 @@ for image_file in image_files:
     results = model(image_file)
 
     # Process results list
-    for i, result in enumerate(results):
+    for result in results:
         boxes = result.boxes  # Boxes object for bounding box outputs
+        print(boxes)
         masks = result.masks  # Masks object for segmentation masks outputs
         keypoints = result.keypoints  # Keypoints object for pose outputs
         probs = result.probs  # Probs object for classification outputs
 
         # Display or save the result
         result.show()  # display to screen
-        result.save(filename=f'result_{i}.jpg')  # save to disk with a unique filename
+        result.save(filename=f'result_{i}.tif')  # save to disk with a unique filename
+        i = i + 1
